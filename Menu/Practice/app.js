f74bde9f9d553f2ee4676424ca98df50e000e059
Bashi-7869 <121685218+Bashi-7869@users.noreply.github.com>
2024-05-25 16:17:33 +0530

    menu item

--- Menu/Practice/app.js
+++ Menu/Practice/app.js
@@ -1,46 +1,46 @@
 const menu = [
     {
-       id:1,
-       title: "japanese souffle pancakes",
-       category: "Breakfast",
-       img: "./images/item1.jpg",
-       price:15.66,
-       desc:` i am s pancake made using soufflé techniques. Egg whites are whipped up with sugar into a glossy thick meringue then mixed with a batter made with the yolks.This panckes are fluffy,jiggley,sweet,soft,so delicious`
- 
-     },
-     {
+        id:1,
+        title: "japanese souffle pancakes",
+        category: "breakfast",
+        img: "/images/item1.jpg",
+        price:15.66,
+        desc:` i am s pancake made using soufflé techniques. Egg whites are whipped up with sugar into a glossy thick meringue then mixed with a batter made with the yolks.This panckes are fluffy and soft.`
+  
+    },
+    {
          id:2,
-         title:"prawnpuff",
-         category:"Breakfast",
-         img:"./images/item2.jpg",
-         price:13.46,
-         desc:` The Prawn Puffs mead with Sift the flour with a teaspoon of salt and baking powder. Mix the butter with the flour and knead into a stiff dough using very little water and dip prwan in dough after that put in oven`
-   
-     },
-     {
-         id:3,
-         title:"sliptdesion breakfast",
-         category:"Breakfast",
-         img:"./images/item3.jpg",
-         price:14.86,
-         desc:` Split Decision Breakfast - Start Your Combos Order Now! 2 eggs*, 2 bacon strips, 2 pork sausage links, 2 buttermilk pancakes & 1 slice of our new Thick Fluffy French Toast`
-   
-     },
-     {
-         id:4,
-         title:"tomoto grilled sandwich",
-         category:"Breakfast",
-         img:'./images/items.jpg',
+         title:"beef-steak",
+         category:"lunch",
+         img:'/images/item5.webp',
          price:16.26,
-         desc:`This is a grilled Cheese and Tomato sandwich version made with tomatoes, cheddar cheese, dried herbs and seasonings`
+         desc:`This is a flat cut of beef with parallel faces, usually cut perpendicular to the muscle fibers with rich spices and vegetables grilled.`
    
-     }
- ]
+    },
+    {
+         id:3,
+         title:"roasted salmon with walnut sauce",
+         category:"dinner",
+         img:'/images/item9.jpg',
+         price:16.26,
+         desc:`Roasted Salmon Recipe is a delicious Fish recipe where fillets of salmon are marinated with Indian spices and then, roasted in the pan to perfection. serve with crust walnut sauce.`
    
- const sectionCenter = document.querySelector(".section-center")
+    },
+    {
+         id:4,
+         title:"lemon cheescake",
+         category:"dessert",
+         img:'/images/item13.jpg',
+         price:16.26,
+         desc:`This simple but refreshing cake brings about a citurs-y breeze with cashew nuts, lemon juice, dates, coconut oil, soy lecithin filling also Crust almonds, maple syrup`
+   
+    },
+]
+   
+const sectionCenter = document.querySelector(".section-center");
 
 window.addEventListener("DOMContentLoaded", () => {
-   displayMenuItems(menu)
+   displayMenuItems(menu);
 
 });
     
@@ -65,4 +65,3 @@ sectionCenter.innerHTML = displayMenu;
 
 };
    
-    
